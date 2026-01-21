7520685be4b74f55cb63dd8dedd0eeb5c9cfad50
aymemn2112 <a.morard@epitech.eu>
2026-01-21 17:35:17 +0100

    add(mvp): home v1

--- mvp/mvp-yowl/src/pages/home/Home.jsx
+++ mvp/mvp-yowl/src/pages/home/Home.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 import "./Home.css";
 
-
 function Home() {
+  const [isVisible, setIsVisible] = useState(false);
 
-    return (
-        <>
-            <div>home page</div>
-        </>
-    )
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
+  return (
+    <>
+      <div className={`outer-card ${isVisible ? 'fade-in' : ''}`}>
+        <div className="inner-content">
+          <p>Outer section content</p>
+          <div className="inner-card">
+            <p>Inner section content</p>
+          </div>
+          <div className="inner-card">
+            <p>Second inner section</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;   
