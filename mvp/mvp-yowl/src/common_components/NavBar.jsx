bad425578e514f25be9f84e191913d630a247b88
AugustinS <58813858+Nitsugua38@users.noreply.github.com>
2026-01-21 16:56:46 +0100

    feat: added mvp main navbar

--- mvp/mvp-yowl/src/common_components/NavBar.jsx
+++ mvp/mvp-yowl/src/common_components/NavBar.jsx
@@ -1,17 +1,25 @@
 import { useState } from 'react'
 import "./NavBar.css";
 
+import home_image from "./assets/home.svg"
+import explore_image from "./assets/explore.svg"
+import notification_image from "./assets/notification.svg"
+import message_image from "./assets/message.svg"
+import profile_image from "./assets/profile.svg"
+import settings_image from "./assets/settings.svg"
+
 import { Link } from 'react-router-dom';
 
 function NavBar() {
     return (
         <>
             <nav className="navbar">
-                <Link to="/">Home</Link><br/>
-                <Link to="/explore">Explore</Link><br/>
-                <Link to="/notifications">Notifications</Link><br/>
-                <Link to="/messages">Messages</Link><br/>
-                <Link to="/profile">Profile</Link>
+                <Link to="/"><img src={home_image}></img><span>Home</span></Link>
+                <Link to="/explore"><img src={explore_image}></img><span>Explore</span></Link>
+                <Link to="/notifications"><img src={notification_image}></img><span>Notifications</span></Link>
+                <Link to="/messages"><img src={message_image}></img><span>Messages</span></Link>
+                <Link to="/profile"><img src={profile_image} className='profileNavImg'></img><span className='profileNavSpan'>Profile</span></Link>
+                <a className='settingsNavIcon'><img src={settings_image}></img><span>Settings</span></a>
             </nav>    
         </>
     )
