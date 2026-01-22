b4df030c061f3075a19b0faa9bf50f4f37eddea0
AugustinS <58813858+Nitsugua38@users.noreply.github.com>
2026-01-22 16:39:48 +0100

    feat: notifications page

--- mvp/mvp-yowl/src/pages/notifications/Notifications.jsx
+++ mvp/mvp-yowl/src/pages/notifications/Notifications.jsx
@@ -1,11 +1,33 @@
 import { useState } from 'react'
 import "./Notifications.css";
 
+import NotifPost from './NotifPost';
+
 function Notifications() {
+
+
+    const [activeTab, setActiveTab] = useState("all");
+
 
     return (
         <>
-            <div>notifications page</div>
+            <div id='TopBar'>
+                <div className={activeTab === "all" ? "activeTAB" : ""} onClick={() => setActiveTab("all")}>All</div>
+                <div className={activeTab === "unread" ? "activeTAB" : ""} onClick={() => setActiveTab("unread")}>Unread</div>
+                <div className={activeTab === "jobs" ? "activeTAB" : ""} onClick={() => setActiveTab("jobs")}>Jobs</div>
+                <div className={activeTab === "follows" ? "activeTAB" : ""} onClick={() => setActiveTab("follows")}>Follows</div>
+                <div className={activeTab === "likes" ? "activeTAB" : ""} onClick={() => setActiveTab("likes")}>Likes</div>
+            </div>
+
+
+            <main>
+                <NotifPost aT={activeTab} />
+                <NotifPost aT={activeTab} />
+                <NotifPost aT={activeTab} />
+                <NotifPost aT={activeTab} />
+                <NotifPost aT={activeTab} />
+                <NotifPost aT={activeTab} />
+            </main>
         </>
     )
 }
