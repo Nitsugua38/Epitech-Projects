3ce0def0c1f39534540b09d52c6c409e6d3c43b6
AugustinS <58813858+Nitsugua38@users.noreply.github.com>
2026-01-23 14:15:42 +0100

    feat: explore page

--- mvp/mvp-yowl/src/pages/explore/Explore.jsx
+++ mvp/mvp-yowl/src/pages/explore/Explore.jsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import "./Explore.css";
+
+import Item from './Item';
 
 function Explore() {
 
+    const [items, setItems] = useState([]);
+    const itemTypes = ["video", "image", "post"]
+
+    useEffect(() => {
+        setItems([ itemTypes[Math.floor(Math.random() * 3)], itemTypes[Math.floor(Math.random() * 3)], itemTypes[Math.floor(Math.random() * 3)], itemTypes[Math.floor(Math.random() * 3)] ]);
+
+        const feed = document.getElementById("feed")
+
+        feed.addEventListener("scroll", () => {
+            if (feed.scrollTop + feed.clientHeight > feed.scrollHeight - 200) {
+                setItems(oldItems => [ ...oldItems, itemTypes[Math.floor(Math.random() * 3)] ])
+            }
+        })
+    }, [])
+
     return (
         <>
-            <div>explore page</div>
+            <div id='feed'>
+                {
+                    items.map(item => (
+                        <Item item={item}/>
+                    ))
+                }
+            </div>
         </>
     )
 }
