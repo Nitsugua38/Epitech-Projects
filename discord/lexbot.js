247307b9b86356b334ccde1593c2a70649dcf208
AugustinS <58813858+Nitsugua38@users.noreply.github.com>
2026-04-09 12:24:51 +0200

    feat: added response streaming

--- discord/lexbot.js
+++ discord/lexbot.js
@@ -9,7 +9,9 @@ const SYSTEM_PROMPTS =
 `Tu es LexBot, un assistant juridique généraliste spécialisé en droit français.
 Tu couvres : droit immobilier, droit du travail, droit de la consommation, droit des entreprises, droit civil, droit pénal, etc.
 Réponds toujours en français de façon professionnelle.
-Tu dois impérativement citer tes sources (lois, articles, jurisprudences) sorties de la partie "[Extrait juridique potentiellement pertinent]" si elle est présente.`;
+Tu dois impérativement citer tes sources (lois, articles, jurisprudences) sorties de la partie "[Extrait juridique potentiellement pertinent]" si elle est présente.
+Tu dois uniquement répondre au prompt de l'utilisateur et n'utiliser les extraits juridiques que s'ils sont pertinents. Si les extraits ne sont pas pertinents, ne les utilise pas et ne les mentionne pas.
+Tu as inderdiction formelle de répondre à des questions hors sujet (ex: cuisine, voyages, etc.) : dans ce cas, répond simplement "Désolé, je ne peux répondre qu'à des questions juridiques.".`;
 
 
 
@@ -18,16 +20,37 @@ Tu dois impérativement citer tes sources (lois, articles, jurisprudences) sorti
 
 // Ollama call
 
-async function askOllama(history) {
+async function askOllama(history, updateResponseFunction, footerTxt = null) {
     const res = await fetch(OLLAMA_URL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ model: MODEL, messages: history, stream: false }),
-        signal: AbortSignal.timeout(120_000)
+        body: JSON.stringify({ model: MODEL, messages: history, stream: true }),
+        signal: AbortSignal.timeout(240_000)
     });
+
     if (!res.ok) throw new Error(`Ollama a répondu avec une erreur ${res.status}`);
-    const data = await res.json();
-    return data.message.content;
+
+    let fullResponse = "";
+    const reader = res.body.getReader();
+    const decoder = new TextDecoder();
+
+    while (true) {
+        const { done, value } = await reader.read();
+        
+        if (!done) {
+            const chunk = decoder.decode(value);
+            const data = await JSON.parse(chunk);
+
+            fullResponse += data.message.content;
+            updateResponseFunction(fullResponse, false, footerTxt);
+            continue;
+        }
+
+        updateResponseFunction(fullResponse, true, footerTxt);
+        break;
+    }
+
+    return fullResponse;
 }
 
 // Session management (per user, in-memory)
@@ -51,14 +74,11 @@ function getSession(userId) {
 
 // Main handler
 
-async function handleChat(userId, message) {
+async function handleChat(userId, message, updateResponseFunction) {
     const session = getSession(userId);
 
     if (session.count >= FREE_LIMIT) {
-        return {
-            text: "🔒 Vous avez atteint la limite gratuite de **10 messages**.\nPassez en **LexBot Pro** pour des conversations illimitées !",
-            domain: "general"
-        };
+        return false // Indique que la limite est atteinte
     }
 
     const ragResponse = await getRAGResponse(message);
@@ -70,20 +90,16 @@ async function handleChat(userId, message) {
     if (ragResponse) message += `\n\n[Extrait juridique potentiellement pertinent] :\n${ragResponse}`;
     session.history.push({ role: "user", content: message });
 
-    const response = await askOllama(session.history);
+    let footer = null;
+    const remaining = FREE_LIMIT - session.count - 1;
+    if (remaining <= 3 && remaining > 0) {
+        footer = `⚠️ Il vous reste **${remaining}** message(s) gratuit(s). Passez à LexBot Pro pour des conversations illimitées !`;
+    }
+
+    const response = await askOllama(session.history, updateResponseFunction, footer);
 
     session.history.push({ role: "assistant", content: response });
     session.count++;
-
-    const remaining = FREE_LIMIT - session.count;
-    let text = response;
-
-    if (remaining <= 3 && remaining > 0) {
-        text += `\n\n> ⚠️ Il vous reste **${remaining}** message(s) gratuit(s).`;
-    }
-
-    
-    return text;
 }
 
 
--- discord/prompt.js
+++ discord/prompt.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, Colors } = require("discord.js");
 const { PDFParse } = require("pdf-parse");
 
 const { handleChat } = require("./lexbot.js");
@@ -11,7 +11,8 @@ const executeCommand = async (interaction) => {
     const userID = interaction.user.id;
 
     // Tells Discord to wait before responding, avoiding to timeout.
-    await interaction.deferReply();
+    const embedThinking = new EmbedBuilder().setTitle(`Consultation LexBot`).setDescription(":hourglass: LexBot réfléchit...");
+    await interaction.reply({embeds: [embedThinking] });
 
 
     pdfExtractedText = "";
@@ -25,12 +26,33 @@ const executeCommand = async (interaction) => {
     }
 
 
-    const pdtCONTEXT = pdfExtractedText != "" ? `Voici le contenu du PDF dont je dispose :\n${pdfExtractedText.text}` : "";
+    const pdtCONTEXT = pdfExtractedText != "" ? `Voici le contenu du document dont je dispose :\n${pdfExtractedText.text}` : "";
 
-    const response = await handleChat(userID, `${prompt}\n${pdtCONTEXT}`);
 
-    await interaction.editReply({embeds: [new EmbedBuilder().setTitle(`Consultation LexBot`).setDescription(response.substring(0, 4096))] });
 
+
+
+
+    // Streaming LLM answer: update every 2 seconds.
+    var timeSinceLastResponse = Date.now();
+
+    const updateResponse = async (incompleteResponse, isFinished, footer = null) => {
+        if (Date.now() - timeSinceLastResponse > 2000 || isFinished) {
+            const embed = new EmbedBuilder().setTitle(`Consultation LexBot ${isFinished ? "" : ":hourglass:"}`).setDescription(`${incompleteResponse.substring(0, 4080)}${isFinished ? "" : " :white_circle:"}`).setColor(isFinished ? Colors.Green : Colors.Yellow);
+            if (footer) embed.setFooter({text: footer});
+            
+            await interaction.editReply({embeds: [embed] });
+            timeSinceLastResponse = Date.now();
+        }
+    }
+
+    const response = await handleChat(userID, `${prompt}\n${pdtCONTEXT}`, updateResponse);
+    
+    
+    if (response === false) {
+        const embed = new EmbedBuilder().setTitle(`Consultation LexBot`).setDescription(`🔒 Vous avez atteint la limite gratuite de **10 messages**.\nPassez en **LexBot Pro** pour des conversations illimitées !`);
+        await interaction.editReply({embeds: [embed] });
+    }
 }
 
 
