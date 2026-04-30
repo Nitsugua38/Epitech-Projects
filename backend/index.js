4973aabf5363f5634429b2224e5474b650ca5dbf
alexander-sudo-rgb <kimdokjagaming11@gmail.com>
2026-04-30 13:50:09 +0200

    feat : did the back,Database Pool,CORS,Route Test

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,31 @@
 require("dotenv").config();
+const mysql = require('mysql2');
+var express = require('express');
+var cors = require('cors');
+var app = express()
 
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT;
+const pool = mysql.createPool({
+  host: 'db',
+  user: "root",
+  database: process.env.DB_NAME,
+  password:process.env.DB_ROOT_PASSWORD,
+});
+
+const promisePool = pool.promise();
 
 
+app.use(cors())
+app.get('/api/test/', function (req, res, next) {
+  res.json({msg: 'Lien obtenu avvec succèes'})
+})
 
 app.use(express.json());
-
-
 
 app.get("/", (req, res) => {
     res.send("hello world");
 });
 
-
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-    console.log("db name is " + process.env.DB_NAME)
-});
+app.listen(process.env.PORT, function () {
+  console.log(`web server listening on port ${process.env.PORT}`)
+  console.log("db name is " + process.env.DB_NAME)
+})
