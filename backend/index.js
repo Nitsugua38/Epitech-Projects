c5042daba00c07ef57209e8194d18a53afb9e292
AugustinS <58813858+Nitsugua38@users.noreply.github.com>
2026-05-12 10:04:26 +0200

    refactor: moved backend routes in separate files

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,11 @@ var cors = require('cors');
 var app = express()
 const bcrypt = require(`bcryptjs`);
 var jwt = require('jsonwebtoken');
+const { jobs } = require("./routes/jobs");
+const { register } = require("./routes/register");
+const { login } = require("./routes/login");
+
+
 
 const pool = mysql.createPool({
   host: 'db',
@@ -16,96 +21,19 @@ const pool = mysql.createPool({
 const promisePool = pool.promise();
 
 app.use(cors())
-
-
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("hello world");
-});
 
-app.get('/api/jobs', (req, res) => {
-  res.json({
-    jobs: [
-      {
-        title: 'Développeur Fullstack JS',
-        company: 'Tech Innovate',
-        location: 'Paris (Hybride)',
-        tags: ['CDI', '45k - 55k'],
-      },
-      {
-        title: 'Ingénieur Data / IA',
-        company: 'DataCorp',
-        location: '100% Remote',
-        tags: ['CDI', 'Python'],
-      },
-      {
-        title: 'Stage Développeur Frontend',
-        company: 'Startup Studio',
-        location: 'Lyon',
-        tags: ['Stage', 'React'],
-      },
-      {
-        title: 'Développeur Backend Python',
-        company: 'FinTech Solutions',
-        location: 'Bordeaux',
-        tags: ['CDI', 'Django'],
-      },
-    ]
-  });
-});
+
+
+
+//jobs list
+app.get('/api/jobs', jobs);
 
 //register
-const register =async (req,res) => {
-  const {name, email, password} = req.body;
-  const salt =await bcrypt.genSalt(10);
-  const hashedPassword =await bcrypt.hash(password, salt);
-  try {
-    await promisePool.query(
-      "INSERT INTO users (email, password, nom, role) VALUES (?, ?, ?, ?)",
-      [email, hashedPassword, name, "user"]
-    );
-    res.status(201).json({ message: 'création réussie' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'erreur lors de la création du compte' });
-  }
-};
 app.post('/api/register', register);
 
-
 //login
-const login = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const [rows]= await promisePool.query("SELECT * FROM users WHERE email = ?", [email]);
-    const user = rows[0];
-    if (!user) {
-      res.status(401).json({ error: 'Invalid credentials' });
-      return;}
-
-    const storedHashedPassword = user.password;
-    const isPasswordValid = await bcrypt.compare(password, storedHashedPassword);
-      
-    if (isPasswordValid) {
-      const payload = {
-        userId: user.id,
-        username: user.prenom + "_" + user.nom,
-      };
-      const secretKey = process.env.JWT_SECRET;
-      const token = jwt.sign(payload, secretKey, {
-        expiresIn: '48h',  
-      });
-      console.log(token);
-       res.status(200).json({ message: 'Login successful voici le token', token: token });
-    } else {
-      res.status(401).json({ error: 'Invalid credentials' });
-    }}
-
-  catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erreur serveur' });}};
-
 app.post('/api/login', login);
 
 
---
+++ backend/routes/register.js
@@ -0,0 +1,20 @@
+const register =async (req,res) => {
+    const {name, email, password} = req.body;
+    const salt =await bcrypt.genSalt(10);
+    const hashedPassword =await bcrypt.hash(password, salt);
+    try {
+        await promisePool.query(
+            "INSERT INTO users (email, password, nom, role) VALUES (?, ?, ?, ?)",
+            [email, hashedPassword, name, "user"]
+        );
+        res.status(201).json({ message: 'création réussie' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'erreur lors de la création du compte' });
+    }
+};
+
+
+module.exports = {
+    register
+}
