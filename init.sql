84eea39bae296de8713cf4bac272402109f13cc5
cyrell <cyrelluneboutros@gmail.com>
2026-05-12 10:46:10 +0200

    feat IA

--- init.sql
+++ init.sql
@@ -1,8 +1,17 @@
 CREATE TABLE users (
   id int PRIMARY KEY AUTO_INCREMENT,
   email varchar(255),
-  password  varchar(255),
-  role varchar (255),
+  password varchar(255),
+  role varchar(255),
   nom varchar(255),
-  prénom varchar(255)
+  prénom varchar(255),
+  profil_recherche text 
+);
+
+CREATE TABLE offers (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  title varchar(255),
+  description text,
+  company varchar(255),
+  location varchar(255)
 );
