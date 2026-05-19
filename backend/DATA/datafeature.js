26bfa275911c6f2fd3b390dc31beec86650b15ad
AugustinS <58813858+Nitsugua38@users.noreply.github.com>
2026-05-19 15:59:01 +0200

    ops: added ci test

--- backend/DATA/datafeature.js
+++ backend/DATA/datafeature.js
@@ -1,6 +1,36 @@
 const { promisePool } = require("../db/db.js");
 const jwt = require("jsonwebtoken");
 
+
+
+
+const normalizeSalary = (salaryString) => {
+
+    if (!salaryString) return "Non précisé";
+
+    const nums = salaryString.match(/\d+/g);
+
+    if (nums && nums.length >= 1) {
+        let value = parseInt(nums[0]);
+
+        if (nums.length >= 2) {
+            value = (parseInt(nums[0]) + parseInt(nums[1])) / 2;
+        }
+
+        if (value < 40) return "Moins de 40k";
+        else if (value < 50) return "40k - 50k";
+        else if (value < 60) return "50k - 60k";
+        else if (value < 70) return "60k - 70k";
+        else return "Plus de 70k";
+    }
+    else {
+        return "Non précisé";
+    }
+};
+
+
+
+
 const getDataFeature = async (req, res) => {
     const authHeader = req.headers.authorization;
 
@@ -50,28 +80,8 @@ const getDataFeature = async (req, res) => {
         };
 
         rows.forEach(row => {
-            
-            if (!row.salary) {
-                salaryDistribution["Non précisé"]++;
-                return;
-            }
-
-            const nums = row.salary.match(/\d+/g);
-            if (nums && nums.length >= 1) {
-
-                let val = parseInt(nums[0]);
-                if (nums.length >= 2) {
-                    val = (parseInt(nums[0]) + parseInt(nums[1])) / 2;
-                }
-
-                if (val < 40) salaryDistribution["Moins de 40k"]++;
-                else if (val < 50) salaryDistribution["40k - 50k"]++;
-                else if (val < 60) salaryDistribution["50k - 60k"]++;
-                else if (val < 70) salaryDistribution["60k - 70k"]++;
-                else salaryDistribution["Plus de 70k"]++;
-            } else {
-                salaryDistribution["Non précisé"]++;
-            }
+            const range = normalizeSalary(row.salary);
+            salaryDistribution[range]++;
         });
 
         res.json({
@@ -87,5 +97,6 @@ const getDataFeature = async (req, res) => {
 };
 
 module.exports = {
-    getDataFeature
+    getDataFeature,
+    normalizeSalary
 };
